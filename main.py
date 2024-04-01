c3543c92427eebfbbbc0253d3b9dbdc9e28b289a
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-04-01 10:14:31 +0100

    My homework

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Welcome to the calculator")
+num1 = int(input("Please pick a number"))
+num2 = int(input("Please pick another number"))
+operation = input("please pick an operator")
+print("The answer is:")
+if operation == "+":
+    print(num1 + num2)
+if operation == "*"
+    print(num1 * num2)
+if operation == -
+    print(num1 - num2)
+if operation == /
+    print(num1 / num2)
